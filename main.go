0ea7549bd58d338f6c373928fc919208b21e9089
Kai Lee <klee@spotsetter.com>
2016-05-23 14:30:31 -0700

    Create simple http server to get file

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"io"
+	"os"
+)
+
+func uploadHandler(writer http.ResponseWriter, request *http.Request) {
+	file, header, error := request.FormFile("file")
+	if error != nil {
+		log.Printf("request: %v; Error: %v;", error, request)
+		return
+	}
+
+	defer file.Close()
+
+	out, error := os.Create("./files/" + header.Filename)
+	if error != nil {
+		log.Printf("Unable to create the file: %v", error)
+		return
+	}
+
+	defer out.Close()
+
+	_, error = io.Copy(out, file)
+	if error != nil {
+		log.Printf("Unable to copy file: %v", error)
+	}
+}
+
+func main() {
+	http.HandleFunc("/upload", uploadHandler)
+	http.ListenAndServe(":8080", nil)
+}
